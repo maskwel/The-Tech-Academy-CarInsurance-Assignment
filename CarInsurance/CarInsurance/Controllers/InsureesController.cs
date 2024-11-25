f40865bba800b3385df9339da703442d7676337b
maskwel <57584511+maskwel@users.noreply.github.com>
2024-11-24 18:21:22 -0800

    Project created CRUD operations added

---
+++ CarInsurance/CarInsurance/Controllers/InsureesController.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using CarInsurance.Data;
+using CarInsurance.Models;
+
+namespace CarInsurance.Controllers
+{
+    public class InsureesController : Controller
+    {
+        private readonly CarInsuranceDbContext _context;
+
+        public InsureesController(CarInsuranceDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Insurees
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Insurees.ToListAsync());
+        }
+
+        // GET: Insurees/Details/5
+        public async Task<IActionResult> Details(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var insuree = await _context.Insurees
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (insuree == null)
+            {
+                return NotFound();
+            }
+
+            return View(insuree);
+        }
+
+        // GET: Insurees/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Insurees/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,FirstName,LastName,EmailAddress,DateOfBirth,CarYear,CarMake,CarModel,DUI,SpeedingTickets,CoverageType,Quote")] Insuree insuree)
+        {
+            if (ModelState.IsValid)
+            {
+                insuree.Id = Guid.NewGuid();
+                _context.Add(insuree);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(insuree);
+        }
+
+        // GET: Insurees/Edit/5
+        public async Task<IActionResult> Edit(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var insuree = await _context.Insurees.FindAsync(id);
+            if (insuree == null)
+            {
+                return NotFound();
+            }
+            return View(insuree);
+        }
+
+        // POST: Insurees/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(Guid id, [Bind("Id,FirstName,LastName,EmailAddress,DateOfBirth,CarYear,CarMake,CarModel,DUI,SpeedingTickets,CoverageType,Quote")] Insuree insuree)
+        {
+            if (id != insuree.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(insuree);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!InsureeExists(insuree.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(insuree);
+        }
+
+        // GET: Insurees/Delete/5
+        public async Task<IActionResult> Delete(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var insuree = await _context.Insurees
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (insuree == null)
+            {
+                return NotFound();
+            }
+
+            return View(insuree);
+        }
+
+        // POST: Insurees/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(Guid id)
+        {
+            var insuree = await _context.Insurees.FindAsync(id);
+            if (insuree != null)
+            {
+                _context.Insurees.Remove(insuree);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool InsureeExists(Guid id)
+        {
+            return _context.Insurees.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ CarInsurance/CarInsurance/Data/CarInsuranceDbContext.cs
@@ -0,0 +1,12 @@
+﻿using CarInsurance.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CarInsurance.Data
+{
+    public class CarInsuranceDbContext : DbContext
+    {
+        public CarInsuranceDbContext(DbContextOptions<CarInsuranceDbContext> options) : base(options) { }
+        
+        public DbSet<Insuree> Insurees { get; set; }
+    }
+}
